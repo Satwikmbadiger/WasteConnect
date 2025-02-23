7bb91f1f372907f08216536511d6e1116dc0514f
aadilraja <aadilraja208@gmail.com>
2025-02-23 18:54:57 +0530

    bhiri

---
+++ product/firebase_config.py
@@ -0,0 +1,10 @@
+# This file contains the firebase configuration details.
+firebaseConfig = {
+  "apiKey": "AIzaSyD152dqaksgSOqK_E7SendiUnmgO1ReRKs",
+  "authDomain": "waste-connect-fb4a1.firebaseapp.com",
+  "projectId": "waste-connect-fb4a1",
+  "storageBucket": "waste-connect-fb4a1.firebasestorage.app",
+  "messagingSenderId": "226399453466",
+  "appId": "1:226399453466:web:5795ea8e3b8796e897eca3",
+  "measurementId": "G-GG2RVRGJQH"
+}
